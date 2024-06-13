65afad83689639bbb34a0106ca9a2aac1ecedb5c
Aditya Chaudhary <VE00YM781@YMI.COM>
2024-06-13 17:42:54 +0530

    add: comments for all

--- src/components/Lander.js
+++ src/components/Lander.js
@@ -1,82 +1,90 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Home.css";
+import "./Home.css"; // Importing the CSS file
 
 const Lander = () => {
-  const [forests, setForests] = useState([]);
-  const [selectedForest, setSelectedForest] = useState("");
-  const [dates, setDates] = useState([]);
-  const [selectedDate, setSelectedDate] = useState("");
-  const [message, setMessage] = useState("");
+  // State variables
+  const [forests, setForests] = useState([]); // State for storing forests
+  const [selectedForest, setSelectedForest] = useState(""); // State for selected forest
+  const [dates, setDates] = useState([]); // State for storing dates
+  const [selectedDate, setSelectedDate] = useState(""); // State for selected date
+  const [message, setMessage] = useState(""); // State for displaying messages
 
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Hook for navigation
 
+  // Fetch forests data when component mounts
   useEffect(() => {
-    // Fetch forests data when component mounts
     fetchForests();
   }, []);
 
+  // Function to fetch forests from the server
   const fetchForests = async () => {
     try {
-      setMessage("");
-      const response = await axios.get("http://127.0.0.1:5000/getForests");
-      setForests(response.data);
+      setMessage(""); // Clear previous messages
+      const response = await axios.get("http://127.0.0.1:5000/getForests"); // API call to fetch forests
+      setForests(response.data); // Update forests state with response data
       if (response.data.length === 0) {
-        setMessage("No data available for forests");
+        setMessage("No data available for forests"); // Display message if no forests found
       }
     } catch (error) {
-      console.error("Error fetching forests:", error);
-      setMessage("Failed to fetch forests data");
+      console.error("Error fetching forests:", error); // Log error to console
+      setMessage("Failed to fetch forests data"); // Display error message
     }
   };
 
+  // Function to handle forest selection change
   const handleForestChange = async (forest) => {
-    setSelectedForest(forest);
+    setSelectedForest(forest); // Update selected forest state
     setDates([]); // Reset dates when forest changes
     try {
-      setMessage("");
+      setMessage(""); // Clear previous messages
       const response = await axios.get(
         `http://127.0.0.1:5000/getDates?forest=${forest}`
-      );
-      setDates(response.data);
+      ); // API call to fetch dates for selected forest
+      setDates(response.data); // Update dates state with response data
       if (response.data.length === 0) {
-        setMessage(`No dates available for ${forest}`);
+        setMessage(`No dates available for ${forest}`); // Display message if no dates found for selected forest
       }
     } catch (error) {
-      console.error("Error fetching dates:", error);
-      setMessage(`Failed to fetch dates for ${forest}`);
+      console.error("Error fetching dates:", error); // Log error to console
+      setMessage(`Failed to fetch dates for ${forest}`); // Display error message
     }
   };
 
+  // Function to handle date selection change
   const handleDateChange = (date) => {
-    setSelectedDate(date);
+    setSelectedDate(date); // Update selected date state
   };
 
+  // Function to handle form submission
   const handleSubmit = async () => {
     try {
-      setMessage("");
+      setMessage(""); // Clear previous messages
       const response = await axios.get(
         `http://127.0.0.1:5000/getReport?forest=${selectedForest}&date=${selectedDate}`
-      );
-      const res = Array(response.data);
-      const dateAndForestData = [selectedForest, selectedDate];
+      ); // API call to fetch report data based on selected forest and date
+      const res = Array(response.data); // Store response data in an array
+      const dateAndForestData = [selectedForest, selectedDate]; // Store selected forest and date
 
-      navigate("/report", { state: { res, dateAndForestData } });
+      navigate("/report", { state: { res, dateAndForestData } }); // Navigate to '/report' route with state
     } catch (error) {
-      console.error("Error fetching report:", error);
-      setMessage("Failed to fetch report data");
+      console.error("Error fetching report:", error); // Log error to console
+      setMessage("Failed to fetch report data"); // Display error message
     }
   };
 
+  // Function to navigate back to the Home page
   const handleReturnHome = () => {
-    navigate("/");
+    navigate("/"); // Navigate to '/' route
   };
 
+  // Render JSX
   return (
     <div className="container-wrapper">
       <div className="container">
         <h1 className="title">Lander Page</h1>
+        {/* Display message if there is any */}
         {message && (
           <p className="message" style={{ color: "red" }}>
             {message}
@@ -91,6 +99,7 @@ const Lander = () => {
               onChange={(e) => handleForestChange(e.target.value)}
             >
               <option value="">-- Select Forest --</option>
+              {/* Map through forests and create options */}
               {forests.map((forest) => (
                 <option key={forest} value={forest}>
                   {forest}
@@ -98,6 +107,7 @@ const Lander = () => {
               ))}
             </select>
           </div>
+          {/* Display date selection if dates are available */}
           {dates.length > 0 && (
             <div className="form-group">
               <label className="form-label">Select Date:</label>
@@ -107,6 +117,7 @@ const Lander = () => {
                 onChange={(e) => handleDateChange(e.target.value)}
               >
                 <option value="">-- Select Date --</option>
+                {/* Map through dates and create options */}
                 {dates.map((date) => (
                   <option key={date} value={date}>
                     {date}
@@ -115,6 +126,7 @@ const Lander = () => {
               </select>
             </div>
           )}
+          {/* Submit button to fetch report */}
           <button
             className="form-button"
             onClick={handleSubmit}
@@ -122,6 +134,7 @@ const Lander = () => {
           >
             Submit
           </button>
+          {/* Button to return to Home page */}
           <button className="form-button" onClick={handleReturnHome}>
             Return to Home
           </button>
--- src/components/Report.js
+++ src/components/Report.js
@@ -1,9 +1,8 @@
 import React from "react";
-import "./Report.css";
-import { useLocation, useNavigate } from "react-router-dom";
+import "./Report.css"; // Importing the CSS file
+import { useLocation, useNavigate } from "react-router-dom"; // Importing necessary hooks
 
-
-
+// List of keys for the report data
 const keysList = [
   "PIC",
   "Ha_sum",
@@ -22,12 +21,15 @@ const keysList = [
 ];
 
 const Report = () => {
+  // Using useLocation and useNavigate hooks from react-router-dom
   const location = useLocation();
   const navigate = useNavigate();
-  const data = location.state.res[0] || [];
-  const forestName = location.state.dateAndForestData[0];
-  const dateFilter = location.state.dateAndForestData[1];
-  console.log(forestName, dateFilter);
+
+  // Extracting data and metadata from location state
+  const data = location.state.res[0] || []; // Extracting report data
+  const forestName = location.state.dateAndForestData[0]; // Extracting forest name
+  const dateFilter = location.state.dateAndForestData[1]; // Extracting date filter
+
   const calculateColumnTotal = (key) => {
     let total = 0;
     data.forEach((item) => {
@@ -35,14 +37,17 @@ const Report = () => {
     });
     return Math.round(total);
   };
+
+  // Function to calculate total of a column with decimal point
   const calculateColumnTotalWithPoint = (key) => {
     let total = 0;
     data.forEach((item) => {
       total += parseFloat(item[key]) || 0; // Ensure to parse to float and handle NaN
     });
-    return total.toFixed(1);
+    return total.toFixed(2); // Return total with 2 decimal places
   };
 
+  // Function to calculate average of a column
   const calculateColumnAverage = (key) => {
     let total = 0;
     let count = 0;
@@ -53,39 +58,29 @@ const Report = () => {
       total += parseFloat(item[key]) || 0; // Ensure to parse to float and handle NaN
     });
     const average = total / count || 0; // Handle division by zero
-    return average.toFixed(1); // Adjust decimal places as needed
+    return average.toFixed(1); // Return average with 1 decimal place
   };
 
-  // const reportData = data[0];
+  // Render JSX
   return (
     <div className="report-container">
       <h1 className="headingMain">Report </h1>
       <div>
+        {/* Table to display report data */}
         <table className="report-table">
           <tbody>
-            <tr
-              style={{
-                fontWeight: "bold",
-              }}
-            >
+            {/* Row for Date */}
+            <tr style={{ fontWeight: "bold" }}>
               <td colSpan={7}>Date</td>
               <td colSpan={7}>{dateFilter}</td>
             </tr>
-            <tr
-              style={{
-                backgroundColor: "#FDFD96",
-                fontWeight: "bold",
-              }}
-            >
+            {/* Row for Forest Name */}
+            <tr style={{ backgroundColor: "#FDFD96", fontWeight: "bold" }}>
               <td colSpan={7}>Forest Name</td>
               <td colSpan={7}>{forestName}</td>
             </tr>
-            <tr
-              style={{
-                backgroundColor: "#FDFD96",
-                fontWeight: "bold",
-              }}
-            >
+            {/* Row for Total Hectares */}
+            <tr style={{ backgroundColor: "#FDFD96", fontWeight: "bold" }}>
               <td colSpan={7}>Total Hectares</td>
               <td colSpan={7}>
                 {(
@@ -95,35 +90,17 @@ const Report = () => {
                 ).toFixed(2)}
               </td>
             </tr>
-
-            <tr
-              style={{
-                backgroundColor: "green",
-                fontWeight: "bold",
-                color: "white",
-              }}
-            >
-              <td
-                colSpan="1"
-                style={{
-                  backgroundColor: "blue",
-                  fontWeight: "bold",
-                  color: "white",
-                }}
-              >
+            {/* Row for Headers of different categories */}
+            <tr style={{ backgroundColor: "green", fontWeight: "bold", color: "white" }}>
+              <td colSpan="1" style={{ backgroundColor: "blue", fontWeight: "bold", color: "white" }}>
                 {dateFilter}
               </td>
               <td colSpan="5">3JS Information</td>
               <td colSpan="4">Rem Information</td>
               <td colSpan="4">QA Information</td>
             </tr>
-            <tr
-              style={{
-                backgroundColor: "blue",
-                fontWeight: "bold",
-                color: "white",
-              }}
-            >
+            {/* Row for Sub-Headers */}
+            <tr style={{ backgroundColor: "blue", fontWeight: "bold", color: "white" }}>
               <td>Names</td>
               <td>ha</td>
               <td>TC</td>
@@ -139,30 +116,18 @@ const Report = () => {
               <td>Min</td>
               <td>Total T</td>
             </tr>
+            {/* Mapping through data to display rows */}
             {[...Array(data.length)].map((_, index) => (
               <tr key={index}>
                 {[...Array(14)].map((_, subIndex) => (
-                  <td
-                    key={subIndex}
-                    style={
-                      subIndex === 0
-                        ? { backgroundColor: "lightgrey", fontWeight: "bold" }
-                        : {}
-                    }
-                  >
+                  <td key={subIndex} style={subIndex === 0 ? { backgroundColor: "lightgrey", fontWeight: "bold" } : {}}>
                     {data[index][keysList[subIndex]] || " "}
-                    {/* Row {index + 1} Col {subIndex + 1} */}
                   </td>
                 ))}
               </tr>
             ))}
-            <tr
-              style={{
-                backgroundColor: "blue",
-                fontWeight: "bold",
-                color: "white",
-              }}
-            >
+            {/* Row for Total calculations */}
+            <tr style={{ backgroundColor: "blue", fontWeight: "bold", color: "white" }}>
               <td>Total</td>
               <td>{calculateColumnTotalWithPoint("Ha_sum")}</td>
               <td>{calculateColumnTotal("Algo_TC_sum")}</td>
@@ -180,6 +145,7 @@ const Report = () => {
             </tr>
           </tbody>
         </table>
+        {/* Button to navigate back to Lander page */}
         <button
           onClick={() => navigate("/lander")}
           style={{
